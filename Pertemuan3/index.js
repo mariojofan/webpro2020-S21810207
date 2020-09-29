a53fde4315f5eb36f7d4fccc5fe1c887c244840a
mariojofan <rioarsjad24@gmail.com>
2020-09-29 22:20:48 +0700

    1st commit

---
+++ Pertemuan3/index.js
@@ -0,0 +1,32 @@
+// console.log("My name is "+fisrtName);
+
+
+// function helloworld(){
+//     var hello = "hello world"; //var = function scope
+//     console.log(hello);
+// }
+//  helloworld();
+
+//1. Variabel dan tipe data
+
+const fisrtName = "John";  //string
+let age = 20; //number
+age = 21;
+const isMarried =true; //bolean
+ 
+
+console.log(typeof fisrtName);
+console.log(typeof aga);
+console.log(isMarried);
+console.log(age+""+isMarried);
+
+const info =
+"My name is "+fisrtName +", i'm  "+ age +" years old"+ isMarried;
+console.log(typeof info); 
+
+// 2.Operrator //
+
+let bilangan =10;
+console.log(bilangan ==="10"); ///Loose Equality
+console.log(bilangan =="10"); // Strict Equality
+
