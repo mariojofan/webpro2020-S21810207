08893a10ccbb2003e626b61bf792082ace75f8e8
mariojofan <rioarsjad24@gmail.com>
2020-09-29 22:33:25 +0700

    7th commit

---
+++ Pertemuan7/index.js
@@ -0,0 +1,90 @@
+// //conditional //
+
+// let nilaiMario = 95;
+
+// if (nilaiMario > 80) {
+//   console.log("SANGAT MEMUASKAN");
+// } else if (nilaiMario >= 60 && nilaiMario <= 80) {
+//   console.log("MEMUASKAN");
+// } else {
+//   console.log("TETAP SEMANGAT, COBA LAGI!");
+// }
+
+
+// //if,else if dan else
+// let nilaiMario = 95;
+
+// if (nilaiMario > 80) {
+//   console.log("SANGAT MEMUASKAN");
+// } else if (nilaiMario >= 60 && nilaiMario <= 80) {
+//   console.log("MEMUASKAN");
+// } else {
+//   console.log("TETAP SEMANGAT, COBA LAGI!");
+// }
+
+// //penggunaan switch case //
+
+
+// let seragamSekolah;
+// let hari = "senin";
+
+// switch (hari) {
+//   case "senin":
+//     seragamSekolah = "kemeja putih bawahan merah";
+//     break;
+//   case "selasa":
+//     seragamSekolah = "kemeja hijau bawahan hitam";
+//     break;
+//   case "rabu":
+//     seragamSekolah = "kemeja putih bawahan putih";
+//     break;
+//   case "kamis":
+//     seragamSekolah = "kemeja batik merah bawahan hitam";
+//     break;
+//   case "jumat":
+//     seragamSekolah = "baju pramuka";
+//     break;
+//   default:
+//     seragamSekolah = "baju bebas";
+// }
+
+
+// //For loop
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// //
+
+// //For in loop
+// const buku = {
+//     judul: "Harry Potter and The Philosopher's Stone",
+//     pengarang: "J. K. Rowling",
+//     tahun: 1997
+//  };
+//  for (x in buku) {
+//    console.log(x, ':', buku[x]);
+//  }
+// //
+// /or of loop
+// const buku = ["Infinity Thanos"}
+// //
+// for (x of buku) {
+//   console.log(x);
+// }
+
+
+// //penggunaan while
+// let i = 1;
+
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// Do while loop
+// let i = 1;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10);
+// 
