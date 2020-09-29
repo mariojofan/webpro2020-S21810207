ece61e0d2461eaa2188c6adf6378b458f43ff434
mariojofan <rioarsjad24@gmail.com>
2020-09-29 22:29:37 +0700

    4th commit

---
+++ Pertemuan6/index.js
@@ -0,0 +1,41 @@
+// Object //
+
+
+//const mahasiswa1 = ["Jhon,"Doe",31,true];
+
+
+//Deklarasi object 
+// const mhs1 = {    //Object Literals
+//     namaDepan: 'Mario',
+//     namaBelakang :"Arsjad",
+//     umur :19,
+//     sudahLulus : false,
+//     alamat: {
+//         jalan : "Amongena satu",
+//         kecamatan : "Langowan Timur",
+//         kabupaten : "Minahasa",
+//     },
+//     IPSemester : [3.05,3.5,3.0,3.4],
+//     hitungIPK: function (){
+//         let total = 0;
+//        this.IPSemester.forEach(function(el){
+//            total = total +el;
+//        });
+//        this.IPKumulatif = total/4;
+//     },
+// };
+ //kata kunci neww
+// const mhs2 = new Object();
+// mhs2.namaDepan = "Jane";
+// mhs2.namaBelakang = "Smith"; 
+
+// Mengakses properti Objek 
+//1. Do notation 
+// console.log (mhs1.umur);
+// //Bracket notation 
+// console.log(mhs1["namaBelakang"]);
+
+// console.log(mhs1.alamat.jalan);
+// console.log(mhs1.IPSemester[2]);
+// mhs1.hitungIPK();
+// console.log(mhs1);
