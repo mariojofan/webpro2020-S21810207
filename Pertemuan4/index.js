846cdba718c94de603cd752a293354d2b0f541b4
mariojofan <rioarsjad24@gmail.com>
2020-09-29 22:24:57 +0700

    2nd commit

---
+++ Pertemuan4/index.js
@@ -0,0 +1,25 @@
+// function //
+
+
+
+    // let LuasA = a * a;
+    // let LuasB = b * b; 
+    
+    // let total = a*a + b*b;
+    
+
+
+    const penjumlahanDuaBujurSangkar = function (a,b){ // Expression
+        return a*a + b*b;
+    };
+    console.log(penjumlahanDuaBujurSangkar(10,3));
+    console.log(penjumlahanDuaBujurSangkar(8,5));
+    
+    
+    // console.log(tambah(kali(5,2),kali(10,2)));
+    // function tambah (a,b) {
+    //     return a+ b;
+    // }
+    // function kali (a,b) {
+    //     return a*b; 
+    // }
