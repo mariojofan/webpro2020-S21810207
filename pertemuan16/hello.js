4e4fa9d0c4847f9d8da76bf60fa12ab2f7d8875d
mariojofan <rioarsjad24@gmail.com>
2020-11-19 13:20:45 +0800

    16th commint

---
+++ pertemuan16/hello.js
@@ -0,0 +1,9 @@
+//export const number = [1,2,3,4,5];
+
+import { word } from './component/word.js';
+
+const hello = (user) => {
+    console.log(`hello, ${user}`);
+}
+
+export default hello;
---
+++ pertemuan16/index.js
@@ -0,0 +1,8 @@
+//import { hello, world, name } from './hello.js'
+//import * as helper from './hello.js'
+//
+//helper.world();
+//console.log(helper.numbers);
+
+import {Func1} from './hello.js'
+Func1('John Doe');
